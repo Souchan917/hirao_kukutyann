412a3ab6d3e92fa2a31f25cb91b1ba9a104553dc
Souchan917 <h.so030502@gmail.com>
2024-11-20 03:56:22 +0900

    a

--- api/chat.js
+++ api/chat.js
@@ -1,4 +1,4 @@
-const fetch = require('node-fetch');
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 module.exports = async (req, res) => {
     if (req.method !== 'POST') {
